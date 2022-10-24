4a6f60e7e630ebece3a22fc4a9e2b7c43bf0067d
fareedkamal <fkamalars@gmail.com>
2022-10-24 15:55:12 +0500

    Updates

---
+++ precache-manifest.4cbcced73728f23c93dd04076e0ecb24.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "3230216c556f3c0e2921",
+    "url": "/resume-app/static/css/main.25048ad5.chunk.css"
+  },
+  {
+    "revision": "3230216c556f3c0e2921",
+    "url": "/resume-app/static/js/main.3230216c.chunk.js"
+  },
+  {
+    "revision": "9990583b20247ba3e22a",
+    "url": "/resume-app/static/js/1.9990583b.chunk.js"
+  },
+  {
+    "revision": "6badf656edb7912f4cbf",
+    "url": "/resume-app/static/js/runtime~main.6badf656.js"
+  },
+  {
+    "revision": "8f7b0113c1644278e9296ec7a7608bd7",
+    "url": "/resume-app/static/media/email.8f7b0113.png"
+  },
+  {
+    "revision": "dc5161dd5e36744d184e0b98e97d31ba",
+    "url": "/resume-app/static/media/man.dc5161dd.png"
+  },
+  {
+    "revision": "9ac5c449563fbab74fe0978e6dcb7818",
+    "url": "/resume-app/index.html"
+  }
+];
